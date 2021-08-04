2dd3cfbe918a83e7cebd008351af73ba8f4ae391
Kevin Austin Holley <holley2032@gmail.com>
2021-08-03 21:59:52 -0400

    Adding some fields, submit button.

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -9,7 +9,13 @@ import {
 } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 
+//Mask password while being typed in.
+
 const SignUp = ({ openSignUp, handleOpenSignUp }) => {
+  const handleSubmitSignUp = () => {
+    handleOpenSignUp();
+    //Add submitting logic to backend.
+  };
   return (
     <div className="SignUp">
       <Button onClick={handleOpenSignUp}>
@@ -17,9 +23,29 @@ const SignUp = ({ openSignUp, handleOpenSignUp }) => {
       </Button>
       <Box className="Form">
         <FormControl>
+          <InputLabel htmlFor="first name">First Name</InputLabel>
+          <Input id="first name"></Input>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="last name">Last Name</InputLabel>
+          <Input id="last name"></Input>
+        </FormControl>
+        <FormControl>
           <InputLabel htmlFor="email address">Email Address</InputLabel>
           <Input id="email address"></Input>
         </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <Input id="password"></Input>
+        </FormControl>
+        <Button
+          onClick={handleSubmitSignUp}
+          size="large"
+          variant="contained"
+          color="primary"
+        >
+          Submit
+        </Button>
       </Box>
     </div>
   );
